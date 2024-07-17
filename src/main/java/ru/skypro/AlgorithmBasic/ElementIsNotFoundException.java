4d858187037f6920072bd8eabc31516516dd5a1a
Максим <bogdanoff.96@yandex.ru>
2024-07-18 01:36:44 +1100

    HW part 1 is done

---
+++ src/main/java/ru/skypro/AlgorithmBasic/ElementIsNotFoundException.java
@@ -0,0 +1,22 @@
+package ru.skypro.AlgorithmBasic;
+
+public class ElementIsNotFoundException extends RuntimeException {
+    public ElementIsNotFoundException() {
+    }
+
+    public ElementIsNotFoundException(String message) {
+        super(message);
+    }
+
+    public ElementIsNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ElementIsNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ElementIsNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
