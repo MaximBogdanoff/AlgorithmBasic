60ab0f6199173e2fb894c82c672b0b58e3f2cae3
Максим <bogdanoff.96@yandex.ru>
2024-07-18 00:47:55 +1100

    Initial Commit

---
+++ src/main/java/ru/skypro/AlgorithmBasic/AlgorithmBasicApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.AlgorithmBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/AlgorithmBasic/AlgorithmBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.AlgorithmBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
